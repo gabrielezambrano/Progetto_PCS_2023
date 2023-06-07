78c1276289a03893e320b82c073af4819ee0eb65
umbertomorzone <127257161+umbertomorzone@users.noreply.github.com>
2023-06-07 13:41:21 +0200

    Update empty_class.hpp

--- Projects/Raffinamento/src/empty_class.hpp
+++ Projects/Raffinamento/src/empty_class.hpp
@@ -21,66 +21,69 @@ using namespace Eigen;
 
 namespace Cells {
 
-class TriangularMesh{
-public:
-    unsigned int numbercell0D;
-    vector<unsigned int> id0D;
-    vector<Vector2d> coordinates0D;
+
+    class Cell0D {
 
 
-    unsigned int numbercell1D;
-    vector<unsigned int> id1D;
-    vector<Vector2i> vertices1D;
-    Eigen::VectorXd LengthEdges = {};
-
-
-    unsigned int numbercell2D;
-    vector<unsigned int> id2D;
-    vector<array<unsigned int, 3>> edges2D;
-    vector<array<unsigned int, 3>> vertices2D;
-    std::vector<vector<unsigned int>> LenghtMax = {};
-
-
-//class Raffinamento{
-//    public:
-//        vector<vector<unsigned int>> MatrAdiac;
-//        bool CreationMatrAdiac();
-//    };    
-    
-};
- class Cell0D {
-    private:
-        unsigned int marker0D;
-
-    public:
-        unsigned int Id0D;
-        Vector2d Coord;
-    Cell0D(unsigned int id,unsigned int marker,Vector2d coord);
-    };
+        public:
+            unsigned int marker0D;
+            unsigned int Id0D;
+            Vector2d Coord;
+        Cell0D(unsigned int id, unsigned int marker, Vector2d coord);
+        };
 
 
     class Cell1D {
-    private:
-        unsigned int marker1D;
 
-    public:
-        unsigned int Id1D;
-        vector<unsigned int> Vertices1D;
-        Cell1D(unsigned int id, unsigned int marker, Vector2i vertices);
-        double LengthEdge();
+        public:
+            unsigned int marker1D;
+            unsigned int Id1D;
+            vector<unsigned int> Vertices1D;
+            Cell1D(unsigned int id, unsigned int marker, Vector2i vertices);
+            double LengthEdge();
  };
 
 
     class Cell2D{
+        public:
+            unsigned int LengthEdges;
+            unsigned int Id2D;
+            array<unsigned int, 3> Vertices2D;
+            array<unsigned int, 3> Edges;
+            Cell2D(unsigned int id,array<unsigned int, 3> Vertices, array<unsigned int, 3> Edges);
+            unsigned int maxedge();
+            double Area();
+        };
+
+    class TriangularMesh{
     public:
-        unsigned int LengthEdges;
-        unsigned int Id2D;
-        array<unsigned int, 3> Vertices2D;
-        array<unsigned int, 3> Edges;
-        Cell2D(unsigned int id,array<unsigned int, 3> Vertices, array<unsigned int, 3> Edges);
-        double MaxEdge();
-        double Area();
+        unsigned int numbercell0D;
+        vector<unsigned int> id0D;
+        vector<Vector2d> coordinates0D;
+        vector<Cells::Cell0D> vectp = {};
+
+
+
+        unsigned int numbercell1D;
+        vector<unsigned int> id1D;
+        vector<Vector2i> vertices1D;
+        vector<Cells::Cell1D> vects = {};
+        vector<double> LengthEdges = {};
+
+
+        unsigned int numbercell2D;
+        vector<unsigned int> id2D;
+        vector<array<unsigned int, 3>> edges2D;
+        vector<array<unsigned int, 3>> vertices2D;
+        std::vector<vector<unsigned int>> LenghtMax = {};
+        vector<Cells::Cell2D> vectt = {};
+
+    };
+
+    class MatrAdiac{
+    public:
+        vector<vector<unsigned int>> Matr;
+        MatrAdiac();
     };
 
  }
-
